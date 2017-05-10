86fd8afc07ed8d10c1113404fb8405f4d58b1b0a
Yehor Chupakhin <egor.chupahin@gmail.com>
2017-05-11 00:54:51 +0300

    nothing

--- js/script.js
+++ js/script.js
@@ -1,27 +1,4 @@
- //
- // // for .menu-left
- //    window.onscroll = function () {
- //        var sun = document.querySelector('#section_sun');
- //        var rocket = document.querySelector('.space-rocket')
- //        var section2 = sun.getBoundingClientRect().bottom - (sun.getBoundingClientRect().bottom*0.1);
- //        var menu = document.querySelector('.menu-left')
- //        if (section2<window.innerHeight){
- //            menu.style.display="block";
- //            rocket.style.display="block";
- //
- //            // menu.style.opacity= 1;
- //            // rocket.style.opacity= 1;
- //
- //        } else {
- //            menu.style.display="none";
- //            rocket.style.display="none";
- //            // menu.style.opacity= 0;
- //            // rocket.style.opacity= 0;
- //        }
- //    };
-// end
-//
-//  for .menu-left
+
     window.onscroll = function () {
         var sun = document.querySelector('#section_sun');
         var rocket = document.querySelector('.space-rocket')
