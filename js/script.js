7a9cdad4b0e92cdb9b51885fe309d7d45b309b6b
Yehor Chupakhin <egor.chupahin@gmail.com>
2017-05-14 23:50:38 +0300

    Animation on Anchor Scroll(class 'go-to' in planet-menu, arrow-down, rocket). Also i did arrow(fa) in the first section

--- js/script.js
+++ js/script.js
@@ -1,19 +1,38 @@
 
+;(function planetMenu() {
     window.onscroll = function () {
         var sun = document.querySelector('#section_sun');
         var rocket = document.querySelector('.space-rocket')
-        var section2 = sun.getBoundingClientRect().bottom - (sun.getBoundingClientRect().bottom*0.1);
-        var menu = document.querySelector('.menu-left')
-        if (section2<window.innerHeight){
-            menu.style.opacity = 1;
-            menu.style.transition = "opacity 3s";
-            menu.style.display="block";
-            rocket.style.display="block";
+        var section2 = sun.getBoundingClientRect().bottom - (sun.getBoundingClientRect().bottom * 0.1);
+        var menu = document.querySelector('.menu-left');
+        var menu_bar = document.querySelector('#menu-bar');
+        if (section2 < window.innerHeight) {
+            if (menu_bar.classList.contains('menu-left') || menu_bar.classList.contains('menu-left-hide')) {
+                menu_bar.classList.add('menu-left-vision');
+                menu_bar.classList.remove('menu-left');
+                menu_bar.classList.remove('menu-left-hide');
+            }
+            rocket.style.display = "block";
         } else {
-            menu.style.opacity = 0;
-            menu.style.transition = "opacity 0.5s";
-            rocket.style.display="none";
+            menu_bar.classList.remove('menu-left-vision');
+            menu_bar.classList.add('menu-left-hide');
+            rocket.style.display = "none";
         }
     };
- // end
+
+}());
+
+;(function animationScroll() {
+    $(document).ready(function() {
+        $('.go-to').on('click',function(){
+            var a_id =$(this).attr("href");
+            $('html, body').animate({
+                scrollTop:$($(a_id)).position().top
+            }, 2000);
+        });
+    });
+}());
+// animationScroll();
+
+
 
